583f1761cf5bb92102b8f5591558ce954e8dc8d5
clownchello <petrov.ivanpetrov246@gmail.com>
2020-03-23 18:37:41 +0200

    i

---
+++ module2/js/task6.js
@@ -0,0 +1,16 @@
+// "use strict";
+
+// let input;
+// let numbers = [];
+// let total = 0;
+// while (true) {
+//   input = prompt("Введите число");
+//   if (input === null) {
+//     for (let i = 0; i < numbers.length; i += 1) {
+//       total += Number(numbers[i]);
+//     }
+//     break;
+//   }
+//   numbers.push(input);
+// }
+// console.log(`Общая сумма чисел равна ${total}`);
